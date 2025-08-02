a233fd52655bc6fb49641a7be3b75894ab063cce
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-02 14:42:38 +0530

    Create 183-CustomersWhoNeverOrder

---
+++ 183-CustomersWhoNeverOrder/183-CustomersWhoNeverOrder.sql
@@ -0,0 +1,4 @@
+-- Last updated: 02/08/2025, 14:42:38
+# Write your MySQL query statement below
+
+select Customers.name as Customers from Customers where Customers.id not in (select customerId from Orders);
