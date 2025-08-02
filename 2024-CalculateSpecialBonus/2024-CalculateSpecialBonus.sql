aef10024c44a2f330b80b2b723975153c0e47432
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-02 14:42:24 +0530

    Create 2024-CalculateSpecialBonus

---
+++ 2024-CalculateSpecialBonus/2024-CalculateSpecialBonus.sql
@@ -0,0 +1,10 @@
+-- Last updated: 02/08/2025, 14:42:23
+# Write your MySQL query statement below
+select employee_id, 
+case when name like 'M%' or name like 'm%' then 0
+else
+  employee_id % 2 * salary
+end  
+as bonus
+from Employees
+order by employee_id;
