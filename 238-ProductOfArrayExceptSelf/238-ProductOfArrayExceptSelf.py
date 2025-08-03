90fe1f29e32c30bd903a2aa30b32c517cd45e451
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-03 13:49:48 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.py
@@ -0,0 +1,28 @@
+# Last updated: 8/3/2025, 1:49:48 PM
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        #  Brute force
+        # import math
+        # prod_arr = []
+        # for i in range(len(nums)):
+        #     sliced_arr = nums[:i]+nums[i+1:]
+        #     prod_arr.append(math.prod(sliced_arr))
+        # return prod_arr
+
+        n = len(nums)
+        prod_arr = [1]*n
+        prefix_product = suffix_product = 1
+        # first pass ----------- calculate prefix product
+        for i in range(n):
+            prod_arr[i] = prefix_product
+            prefix_product *= nums[i]
+
+        # second pass --------- calculate suffix product
+        for i in range(n-1, -1, -1):
+            prod_arr[i]*=suffix_product
+            suffix_product *= nums[i]
+
+        return prod_arr
+
+
