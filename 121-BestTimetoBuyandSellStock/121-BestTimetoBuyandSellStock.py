dd5cfa89a18fe1fb66f4d314277473fd8e324dcd
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-03 13:49:18 +0530

    File updated at 8/3/2025, 1:49:18 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,28 +1,12 @@
-# Last updated: 8/3/2025, 1:47:32 PM
+# Last updated: 8/3/2025, 1:49:18 PM
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def isPalindrome(self, s: str) -> bool:
+        trimmed_str = ''.join(char.lower() for char in s if char.isalnum())
+        l,r=0,len(trimmed_str)-1
+        while l<r:
+            if trimmed_str[l] != trimmed_str[r]:
+                return False
+            l+=1
+            r-=1
+        return True
         
-        #  Brute force
-        # import math
-        # prod_arr = []
-        # for i in range(len(nums)):
-        #     sliced_arr = nums[:i]+nums[i+1:]
-        #     prod_arr.append(math.prod(sliced_arr))
-        # return prod_arr
-
-        n = len(nums)
-        prod_arr = [1]*n
-        prefix_product = suffix_product = 1
-        # first pass ----------- calculate prefix product
-        for i in range(n):
-            prod_arr[i] = prefix_product
-            prefix_product *= nums[i]
-
-        # second pass --------- calculate suffix product
-        for i in range(n-1, -1, -1):
-            prod_arr[i]*=suffix_product
-            suffix_product *= nums[i]
-
-        return prod_arr
-
-
