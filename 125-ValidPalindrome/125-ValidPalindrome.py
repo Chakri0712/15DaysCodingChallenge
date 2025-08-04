990d11f42eb0e6f21a4478916462592a26a49b7d
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-04 16:03:40 +0530

    File updated at 8/4/2025, 4:03:40 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,6 +1,11 @@
-# Last updated: 8/4/2025, 11:08:30 AM
-from collections import Counter
-
+# Last updated: 8/4/2025, 4:03:40 PM
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output_dict = defaultdict(list)
+        n = len(strs)
+        if n==1:
+            return [strs]
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            output_dict[sorted_word].append(word)
+        return list(output_dict.values())
