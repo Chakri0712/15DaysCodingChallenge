d2c0d27a66d95371f268ff4ec98c15fb29ff779e
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-04 11:06:34 +0530

    File updated at 8/4/2025, 11:06:34 AM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,12 +1,4 @@
-# Last updated: 02/08/2025, 14:42:39
+# Last updated: 8/4/2025, 11:06:34 AM
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        trimmed_str = ''.join(char.lower() for char in s if char.isalnum())
-        l,r=0,len(trimmed_str)-1
-        while l<r:
-            if trimmed_str[l] != trimmed_str[r]:
-                return False
-            l+=1
-            r-=1
-        return True
-        
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
