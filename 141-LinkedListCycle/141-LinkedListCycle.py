52f586921b8e1f040a94ba553b47f39d4f3b3240
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-06 14:40:20 +0530

    File updated at 8/6/2025, 2:40:20 PM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -1,4 +1,4 @@
-# Last updated: 8/6/2025, 12:10:14 PM
+# Last updated: 8/6/2025, 2:40:20 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -8,16 +8,30 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
-        # Brute Force (using set)
         if not head:
             return False
-        visited_nodes = set()
-        current = head
-        while current is not None:
-            if current in visited_nodes:
-                return True
-            visited_nodes.add(current)
-            current = current.next
 
+        # Brute Force (using set)
+
+        # visited_nodes = set()
+        # current = head
+        # while current is not None:
+        #     if current in visited_nodes:
+        #         return True
+        #     visited_nodes.add(current)
+        #     current = current.next
+
+        # return False
+
+        # Floyd cycle detection (tortoise-hare algorithm)
+
+        hare=tortoise=head
+        while hare and hare.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return True
         return False
+
+
         
