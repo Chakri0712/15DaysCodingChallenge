279e5fa569c95297b23e2449f4977a14a46e663e
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-07 14:27:05 +0530

    File updated at 8/7/2025, 2:27:04 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,15 +1,15 @@
-# Last updated: 8/7/2025, 11:53:07 AM
+# Last updated: 8/7/2025, 2:27:04 PM
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        n=len(nums)
-        low,high=0,n-1
-        while low <= high:
-            mid = low + (high-low) // 2
-            if target == nums[mid]:
-                return mid
-            if target < nums[mid]:
-                high = mid - 1
-            elif target > nums[mid]:
-                low = mid + 1
-        return low
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n=len(numbers)
+        low,high,sum=0,n-1,0
+        while low<=high:
+            sum = numbers[low]+numbers[high]
+            if target == sum:
+                return [low+1, high+1]
+            if sum > target:
+                high -=1
+            elif sum < target:
+                low+=1
         
+        return [0,0]
