6cdf346abdff65736633affbea85c219389b7291
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-08 12:40:48 +0530

    File updated at 8/8/2025, 12:40:48 PM

--- 81-SearchinRotatedSortedArrayII/81-SearchinRotatedSortedArrayII.py
+++ 81-SearchinRotatedSortedArrayII/81-SearchinRotatedSortedArrayII.py
@@ -1,5 +1,27 @@
-# Last updated: 8/8/2025, 12:26:25 PM
+# Last updated: 8/8/2025, 12:40:48 PM
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        return target in nums
-        
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            curr = nums[mid]
+            if curr == target or nums[left] == target or nums[right] == target:
+                return True
+            elif curr == nums[left] and curr == nums[right]:
+                left += 1
+                right -= 1
+            elif curr >= nums[left]:
+                # left side sorted
+                if target < nums[left] or target > curr:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                # right side sorted
+                if target > nums[right] or target < curr:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+        return False
