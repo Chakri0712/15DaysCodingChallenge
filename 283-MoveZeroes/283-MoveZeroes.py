0055c78110a39545374681587f90b5a85456fcde
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-14 14:57:02 +0530

    File updated at 8/14/2025, 2:57:02 PM

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,10 +1,14 @@
-# Last updated: 02/08/2025, 14:42:35
+# Last updated: 8/14/2025, 2:57:02 PM
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         l=r=0
-        n = len(nums)
-        while r<n:
-            if nums[r] != 0:
+        for r in range(len(nums)):
+            if nums[l] == 0 and nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l+=1
-            r+=1        
+            elif nums[l] != 0:
+                l+=1
+        
