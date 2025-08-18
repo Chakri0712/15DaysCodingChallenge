50739cf8b49863ab43301249ace1e166dd11d8ee
chakradhar <68968048+Chakri0712@users.noreply.github.com>
2025-08-18 09:22:43 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.py
@@ -0,0 +1,7 @@
+# Last updated: 8/18/2025, 9:22:44 AM
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        return target in nums
+
+    # The actual way of doing this is binary search with left sort and right sort verificaton
+        
